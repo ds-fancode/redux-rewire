2503c38c8ba03544cc3493d29d2e21da931376c3
Bishal Biswas <bishal@fancode.com>
2023-06-01 08:26:18 +0530

    feat(readme): readme copy to publish

--- publish.js
+++ publish.js
@@ -7,11 +7,14 @@ newPackageJson.types = './esm/index.d.ts'
 delete newPackageJson.scripts
 delete newPackageJson.devDependencies
 
-newPackageJson.dependencies = {}
+newPackageJson.dependencies = {
+  'react-redux': '*',
+  "immer": "*",
+  redux: '*',
+}
 newPackageJson.peerDependencies = {
   ...newPackageJson.peerDependencies,
-  'react-redux': '^8.0.5',
-  redux: '^4.2.0',
   typescript: '^4.9.4'
 }
 fs.writeFileSync('./lib/package.json', JSON.stringify(newPackageJson))
+fs.copyFileSync('./README.md', './lib/README.md')
