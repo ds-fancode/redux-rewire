ab069ff5420add88b8217bfb102c4c6a1f6fb186
kamleshsharma <kamlesh.sharma@fancode.com>
2023-10-31 14:24:07 +0530

    chore: test runner fixes

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,22 @@
 module.exports = {
   // setupFilesAfterEnv: ['./jest.mock.js'],
   // Only match files that strictly ends with __tests__.ts or spec.ts
-  testRegex: "\\.(__tests__|spec)\\.ts$",
+  testRegex: '\\.(test|__tests__|spec)\\.ts$',
   testMatch: null,
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  collectCoverageFrom: ["src/**/**.(ts|tsx)", "!src/**/**.d.ts"],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  setupFiles: [
-    // '../../jest.setup.ts'
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  collectCoverageFrom: ['src/**/**.(ts|tsx)', '!src/**/**.d.ts'],
+  setupFiles: [],
+  testEnvironment: 'jsdom',
+  bail: true,
+  verbose: true,
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
   ],
-};
+  setupFilesAfterEnv: ['<rootDir>/jest.mock.ts'],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
+      'identity-obj-proxy',
+  },
+}
