2dc84f783dea6ac8d3a8a4c29e872c7fc54b4148
kamleshsharma <kamlesh.sharma@fancode.com>
2024-08-04 22:48:18 +0530

    fix: prettier

--- example/todo-app/config/webpackDevServer.config.js
+++ example/todo-app/config/webpackDevServer.config.js
@@ -124,10 +124,5 @@ module.exports = function (proxy, allowedHost) {
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
       devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath))
     },
-    resolve: {
-      alias: {
-        'redux-rewire': path.resolve(process.cwd(), '../../src'),
-      },
-    },
   }
 }
