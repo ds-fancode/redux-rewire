150b7754f6bb3153a9f34b23528f4e48ab1d5dc7
kamleshsharma <kamlesh.sharma@fancode.com>
2024-08-14 11:11:55 +0530

    fix: rename few props

--- src/helper/keys-selector.ts
+++ src/helper/keys-selector.ts
@@ -1,6 +1,6 @@
 type IKeysSelector = <
-  InputKeys extends keyof Obj,
-  Obj extends {[key in string]: any}
+  Obj extends {[key in string]: any},
+  InputKeys extends keyof Obj
 >(
   keys: InputKeys[]
 ) => (state: Obj) => {[Key in InputKeys]: Obj[Key]}
@@ -12,16 +12,3 @@ export const keysSelector: IKeysSelector = function (keys) {
       return acc
     }, {} as any)
 }
-export const keysSelector2 = <State>(state: State) => {
-  return <InputKeys extends keyof State>(
-    keys: InputKeys[]
-  ): {[key in InputKeys]: State[key]} =>
-    keys.reduce((acc, currentValue) => {
-      acc[currentValue] = state[currentValue]
-      return acc
-    }, {} as any)
-}
-
-const state = {car: 10, bike: 20}
-// const result = keysSelector(['cars'])(state)
-const result2 = keysSelector2(state)(['car'])
